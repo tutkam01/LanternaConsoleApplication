3315a74a26eae650cbf99330e46668bb7fb31fef
Mateusz Tutka <mateusz.tutka@gmail.com>
2015-04-17 20:54:52 +0200

    handle of space bar second part

--- src/main/java/tutka/mateusz/keys/CharacterKeyHandler.java
+++ src/main/java/tutka/mateusz/keys/CharacterKeyHandler.java
@@ -1,8 +1,12 @@
 package tutka.mateusz.keys;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 
 import tutka.mateusz.interfaces.KeyHandler;
 import tutka.mateusz.models.Position;
@@ -20,6 +24,7 @@ public class CharacterKeyHandler implements KeyHandler {
 		userTerminal.shiftCaret();
 		if(userTerminal.isCurrentKeySpacebar(keyToHandle)){
 			userTerminal.handleKeyWords(keyToHandle);
+			handleCommandShift(userTerminal);
 		}else {
 			userTerminal.getWord().addKey(keyToHandle);						
 		}
@@ -29,7 +34,25 @@ public class CharacterKeyHandler implements KeyHandler {
 	}
 	
 	private void handleCommandShift(UserTerminal userTerminal){
+		
+
 		if(userTerminal.getCaret().getPosition().compareTo(userTerminal.getCurrentCommand().getPositionKeyMap().lastKey())<=0){
+			TreeMap<Position, KeyStroke> afterShift = new TreeMap<Position, KeyStroke>();
+			SortedMap<Position, KeyStroke> notShiftedPart = userTerminal.getCurrentCommand().getPositionKeyMap().headMap(new Position(userTerminal.getCaret().getX(), userTerminal.getCaret().getY()));
+			afterShift.putAll(notShiftedPart);
+			
+			afterShift.put(new Position(userTerminal.getCaret().getX(), userTerminal.getCaret().getY()), new KeyStroke(' ', false, false));
+			
+			SortedMap<Position, KeyStroke> shiftedPart = userTerminal.getCurrentCommand().getPositionKeyMap().tailMap(new Position(userTerminal.getCaret().getX(), userTerminal.getCaret().getY()));
+			
+			for(Map.Entry<Position, KeyStroke> entry: shiftedPart.entrySet()){
+				Position newPosition = new Position(entry.getKey().getX() + 1, entry.getKey().getY());
+				afterShift.put(newPosition, entry.getValue());
+				//zmenic polozenie kursora?
+				userTerminal.getTerminal().putCharacter(entry.getValue().getCharacter());
+			}
+			userTerminal.getTerminal().setCursorPosition(userTerminal.getCaret().getX(), userTerminal.getCaret().getY());
+			userTerminal.getCurrentCommand().getPositionKeyMap().putAll(afterShift);
 			
 		}
 	}
