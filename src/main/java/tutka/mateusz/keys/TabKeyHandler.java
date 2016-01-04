b829151047dcd42acba70bd7beac9224b648db3f
Mateusz Tutka <mateusz.tutka@gmail.com>
2016-01-04 20:36:59 +0100

    ensuring that only unique matches will be handled

--- src/main/java/tutka/mateusz/keys/TabKeyHandler.java
+++ src/main/java/tutka/mateusz/keys/TabKeyHandler.java
@@ -9,22 +9,34 @@ public class TabKeyHandler implements KeyHandler {
 
 	public void handleKey(KeyStroke keyToHandle, UserTerminal userTerminal) {
 		
+		String partialString = userTerminal.returnString();
+		String match = "";
+		int numberOfMatches = 0;
+		
+		if(partialString.isEmpty()) return;
+		
 		for(String keyWord: userTerminal.getKeyWords()){
-			String partialString = userTerminal.returnString();
-			if(!partialString.isEmpty() && keyWord.startsWith(partialString)){
-				
-				userTerminal.putString(keyWord);
-				
-				for(int i = 0; i < keyWord.length() - partialString.length() + 1; i++){
-					userTerminal.shiftCaret();
-				}
-				
-				userTerminal.getWord().resetWord();
-				
-				return;
+			if(keyWord.startsWith(partialString)){
+				numberOfMatches++;
+				match = keyWord;
 			}
+		}
+		
+		if(numberOfMatches == 1){
+			userTerminal.putString(match);
+			int numberOfRemainingCharacters = getNumberOfRemainingCharacters(partialString, match);
+			
+			for(int i = 0; i < numberOfRemainingCharacters; i++){
+				userTerminal.shiftCaret();
+			}
+			
+			userTerminal.getWord().resetWord();
 		}
 		
 	}
 
+	private int getNumberOfRemainingCharacters(String partialString, String match) {
+		return match.length() - partialString.length() + 1;
+	}
+	
 }
